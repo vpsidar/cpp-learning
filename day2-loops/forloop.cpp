73be8eca57707bce11c408d5cdfc073e5e6a9001
vijay <vijayprakash.s@kevells.com>
2022-06-18 23:08:12 +0530

    break statemetn implementation

--- day2-loops/forloop.cpp
+++ day2-loops/forloop.cpp
@@ -2,12 +2,19 @@
 using namespace std;
 
 int main(){
+    int no_of_odds = 0;
     for (int i = 0; i < 100; i++)
-    {
+    {   
+        if (no_of_odds == 10)
+        {
+            break;
+        }
+        
         if(i % 2 == 0) {
             continue;
         }
         cout<<i<<endl;
+        no_of_odds++;
     }
     
     return 0;
