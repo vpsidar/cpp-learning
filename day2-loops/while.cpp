3c3591d74d227d6dc726b5aaf8597e8c6f067b6f
vijay <vijayprakash.s@kevells.com>
2022-06-18 22:59:22 +0530

    while loop implemented

---
+++ day2-loops/while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //cout<<"hello world";
+    int n = 0;
+    while (n<10)
+    {
+        cout<<n<<endl;
+        n++;
+    }
+    
+    return 0;
+}
