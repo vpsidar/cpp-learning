00ce5e1f30b6515ad261570da637fe2cb3162e4b
vijay <vijayprakash.s@kevells.com>
2022-06-18 23:02:13 +0530

    dowhile implementation

---
+++ day2-loops/dowhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //cout<<"hello world";
+    int n = 0;
+    do
+    {
+        cout<<"proagram stared";
+        n++;
+    } while (n <1);
+    
+   
+    return 0;
+}
