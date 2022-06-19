c646bd27cc879d89b623062a8c8d8cc51db27640
vijay <vijayprakash.s@kevells.com>
2022-06-19 10:03:01 +0530

    pattern and funtions

--- day4-patterns/floydtriangle.cpp
+++ day4-patterns/floydtriangle.cpp
@@ -4,16 +4,14 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
+    int counter = 0;
     for (int i = 1; i<= n; i++)
     {
-        for (int j = 0; j <= n ; j++)
+        for (int j = 1; j <=i; j++)
         {
-            if(j <= n-i){
-                cout<<"  ";
-            } else {
-                cout<<"* ";
-            }
+            cout<<++counter<<" ";
         }
+        
         cout<<endl;
         
     }
--- day4-patterns/numpyramid.cpp
+++ day4-patterns/numpyramid.cpp
@@ -6,14 +6,11 @@ int main() {
     cin>>n;
     for (int i = 1; i<= n; i++)
     {
-        for (int j = 0; j <= n ; j++)
+        for (int j = 1; j <=i; j++)
         {
-            if(j <= n-i){
-                cout<<"  ";
-            } else {
-                cout<<"* ";
-            }
+            cout<<i<<" ";
         }
+        
         cout<<endl;
         
     }
