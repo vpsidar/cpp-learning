abc031278b4804cf4042d9b568c5ea3e9a4f0154
Vijay Prakash <vpmsidar@gmail.com>
2022-09-18 13:35:30 +0530

    checkDuplicte

---
+++ day8/checkBound.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 2147483647;
+    int b = 2;
+    int c = a+b;
+    cout<<"Hello World "<< c<<endl;
+    cout<< c % 5;
+    return 0;
+}
---
+++ day8/checkDuplicate.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+vector<int> checkDuplicate(int arr[],int n){
+    vector<int> ans;
+    return ans;
+}
+int main() {
+
+    return 0;
+}
